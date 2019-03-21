fa6fe10bb98b147b1e2169dfd170cc77092e7a40
JamesRileydev <jamesriley.dev@gmail.com>
2019-03-20 21:17:04 -0400

    Added logic in TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from "../src/components/TodoComponents/TodoList";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -6,8 +7,9 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="todoListMain">
+        <h2>Todo App</h2>
+        <TodoList />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,46 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from 'react';
+
+export default class TodoList extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            userInput: '',
+            list: []
+        }
+    }
+
+    changeUserInput(input) {
+        this.setState({
+            userInput: input
+        }, ()=> console.log(input));
+    }
+
+    addToList(input){
+        let listArray = this.state.list; //clone of list:[]
+
+        listArray.push(input);
+        this.setState({
+            list: listArray,
+            userInput: ''
+        })
+    }
+
+    render() {
+        return(
+            <div className="todo-list-main">
+                <input
+                 onChange= { (e) => this.changeUserInput(e.target.value)}
+                 value={this.state.userInput} type="text" 
+                 />
+                <button onClick={()=> this.addToList(this.state.userInput)}>Add Item</button>
+                <button>Clear Completed</button>
+                <ul>
+                    {this.state.list.map((val) => <li>{val}</li>)}
+                </ul>
+            </div>
+        )
+    }
+}
